f7f981f18a78c958b41c6139d92af3309cc35f1b
mattmori16 <mateus5310@live.com>
2026-01-27 18:18:51 -0300

    Base do projeto WMS Mori - Sistema de Gerenciamento de Armazém

---
+++ src/main/java/com/mori/wms/WmsMoriApplication.java
@@ -0,0 +1,13 @@
+package com.mori.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsMoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmsMoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mori/wms/WmsMoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mori.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsMoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
